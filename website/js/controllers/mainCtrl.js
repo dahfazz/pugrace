54422cf03143febae3dc539b8efaaf7c6db0fffa
Fabien Zibi <dahfazz@gmail.com>
2014-09-24 14:10:15 +0200

    website

--- website/js/controllers/mainCtrl.js
+++ website/js/controllers/mainCtrl.js
@@ -1,5 +1,9 @@
 website.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
 
+
+    $scope.url_github  = 'https://github.com/dahfazz/pugrace';
+    $scope.url_twitter = 'https://twitter.com/_faz';
+
     $http({
         'url': '/stats',
         'method': 'GET'
