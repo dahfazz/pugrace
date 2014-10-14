2113d9a68fccded0619899f568c71619090d092c
Fabien Zibi <dahfazz@gmail.com>
2014-10-14 16:24:25 +0200

    leaderboard

--- src/controllers/LeaderboardCtrl.js
+++ src/controllers/LeaderboardCtrl.js
@@ -1,12 +1,15 @@
 myApp.controller('LeaderboardCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.$id = 'leaderboardCtrl';
+    $scope.array = [];
 
     $http({
             'method': 'GET',
             'url': '/allrunners'
         }).success(function(data){
-            $scope.RUNNERS = data;
-        });
 
+            angular.forEach(data, function(value, key) {
+                $scope.array.push(value);
+            });
+        });
 }]);
