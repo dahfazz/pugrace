9efbf1d0ce108c092daa9ea5ce7184b61e47edb3
fabien zibi <fabien.zibi@gmail.com>
2024-11-14 16:02:16 +0100

    chore: more questions

--- questions.js
+++ questions.js
@@ -17,6 +17,73 @@ var questions = [
         "question": "Taille de Bam Adebayo",
         "answer": 0
     },
+    {
+        "type": "open",
+        "options": ["20", "22", "24", "26"],
+        "question": "Numéro d'Andrew Wiggins aux Warriors",
+        "answer": 1
+    },
+    {
+        "type": "open",
+        "options": [57, 58, 59, 60],
+        "question": "Nb de victoires d'OKC la saison passée",
+        "answer": 0
+    },
+    {
+        "type": "open",
+        "options": [16000, 17000, 18000, 19000 ],
+        "question": "Nb de place au Intuit Dome, nouvelle salle des Clippers",
+        "answer": 2
+    },
+    {
+        "type": "open",
+        "options": [2012, 2014, 2015, 2016],
+        "question": "En quelle année est drafté Jokic",
+        "answer": 1
+    },
+    {
+        "type": "open",
+        "options": [2, 3, 4, 5],
+        "question": "A quel choix est drafté James Harden en 2009",
+        "answer": 1
+    },
+    {
+        "type": "open",
+        "options": [57, 58, 59, 60],
+        "question": "Nb de victoires d'OKC la saison passée",
+        "answer": 0
+    },
+    {
+        "type": "open",
+        "options": [1.95, 1.96, 1.97, 1.98],
+        "question": "Taille officielle de Bilal Coulibaly",
+        "answer": 3
+    },
+    {
+        "type": "open",
+        "options": [17, 18, 19, 20],
+        "question": "Combien de saisons pour Chris Paul en NBA",
+        "answer": 2
+    },
+    {
+        "type": "open",
+        "options": [30, 31, 32, 33],
+        "question": "Quel âge a Dennis Schroder",
+        "answer": 1
+    },
+    {
+        "type": "open",
+        "options": ["Moka Center", "Target Center", "Spectrum arena", "Moda Center"],
+        "question": "Nom de la salle des Trailblazers",
+        "answer": 3
+    },
+    {
+        "type": "open",
+        "options": [9, 10, 11, 12],
+        "question": "Combien de fois Curry a été All-Star",
+        "answer": 1
+    },
+    
 ];
 
 
